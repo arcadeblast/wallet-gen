72e0e4cda0c4ce55388ead8520b65b570f897d56
arcadeblast <tfraking@gmail.com>
2021-03-28 21:57:27 -0400

    Added public key generation.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'ecdsa', '~> 1.2'
--- wallet-gen.rb
+++ wallet-gen.rb
@@ -1,6 +1,17 @@
+require 'ecdsa'
+
 SEED = ARGV[0] || Random.new_seed
 MAX = 115792089237316195423570985008687907852837564279074904382605163141518161494336
-BASE = 16
+HEX = 16
 gen = Random.new(SEED.to_i)
 
-puts gen.rand(MAX).to_s(BASE)
+private_key_base10 = gen.rand(MAX)
+
+group = ECDSA::Group::Secp256k1
+point = group.generator.multiply_by_scalar(private_key_base10)
+public_key_base16 = ECDSA::Format::PointOctetString.encode(point, compression: true).unpack('H*').first
+
+puts 'Private Key:'
+puts private_key_base10.to_s(HEX)
+puts 'Public Key:'
+puts public_key_base16
