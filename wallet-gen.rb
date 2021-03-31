83a294d9a13fd1ef78807d78071d09b84dad4d2d
arcadeblast <tfraking@gmail.com>
2021-03-30 22:26:04 -0400

    Extracting private key gen into its own method. Other refactors for readability.

--- wallet-gen.rb
+++ wallet-gen.rb
@@ -1,20 +1,13 @@
 require 'ecdsa'
 
-SEED = ARGV[0] || Random.new_seed
 MAX = 115792089237316195423570985008687907852837564279074904382605163141518161494336
 HEX = 16
-gen = Random.new(SEED.to_i)
 
-private_key_base10 = gen.rand(MAX)
-
-group = ECDSA::Group::Secp256k1
-point = group.generator.multiply_by_scalar(private_key_base10)
-public_key_base16 = ECDSA::Format::PointOctetString.encode(point, compression: true).unpack('H*').first
-
-puts 'Private Key:'
-puts private_key_base10.to_s(HEX)
-puts 'Public Key:'
-puts public_key_base16
+def gen_private_key(seed)
+  seed = seed || Random.new_seed
+  r = Random.new(seed.to_i)
+  r.rand(MAX).to_s(HEX)
+end
 
 def hash160(hex)
   bytes = [hex].pack("H*")
@@ -50,5 +43,15 @@ def int_to_base58(int_val, leading_zero_bytes=0)
   base58_val
 end
 
+private_key = gen_private_key(ARGV[0])
+
+group = ECDSA::Group::Secp256k1
+point = group.generator.multiply_by_scalar(private_key.to_i(HEX))
+public_key = ECDSA::Format::PointOctetString.encode(point, compression: true).unpack('H*').first
+
+puts 'Private Key:'
+puts private_key
+puts 'Public Key:'
+puts public_key
 puts 'Public Address:'
-puts hash160_to_address(hash160(public_key_base16))
+puts hash160_to_address(hash160(public_key))
