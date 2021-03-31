d5b8a1510d9da2d50fbd19aa38095a18dbd70c96
arcadeblast <tfraking@gmail.com>
2021-03-30 21:11:16 -0400

    Added public address generation.

--- wallet-gen.rb
+++ wallet-gen.rb
@@ -15,3 +15,40 @@ puts 'Private Key:'
 puts private_key_base10.to_s(HEX)
 puts 'Public Key:'
 puts public_key_base16
+
+def hash160(hex)
+  bytes = [hex].pack("H*")
+  Digest::RMD160.hexdigest Digest::SHA256.digest(bytes)
+end
+
+def hash160_to_address(hex)
+  encode_address hex, '1e'
+end
+
+def encode_address(hex, version)
+  hex = version + hex
+  encode_base58(hex + checksum(hex))
+end
+
+def encode_base58(hex)
+  leading_zero_bytes  = (hex.match(/^([0]+)/) ? $1 : '').size / 2
+  ('1' * leading_zero_bytes) + int_to_base58( hex.to_i(16) )
+end
+
+def checksum(hex)
+  b = [hex].pack("H*") # unpack hex
+  Digest::SHA256.hexdigest( Digest::SHA256.digest(b) )[0...8]
+end
+
+def int_to_base58(int_val, leading_zero_bytes=0)
+  alpha = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+  base58_val, base = '', alpha.size
+  while int_val > 0
+    int_val, remainder = int_val.divmod(base)
+    base58_val = alpha[remainder] + base58_val
+  end
+  base58_val
+end
+
+puts 'Public Address:'
+puts hash160_to_address(hash160(public_key_base16))
